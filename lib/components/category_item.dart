b64bcb6fe822e330559ef540d3641d4c523d7aac
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-03-25 05:58:55 -0300

    Tela de Categorias

---
+++ lib/components/category_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(category.title),
+    );
+  }
+}
