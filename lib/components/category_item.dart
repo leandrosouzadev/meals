7a6e0eecee833cc73792ae8925ee443724f32122
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-03-26 05:17:36 -0300

    Passando dados via construtor

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -14,7 +14,7 @@ class CategoryItem extends StatelessWidget {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) {
-          return const CategoriesMealsScreen();
+          return CategoriesMealsScreen(category);
         },
       ),
     );
